9d6606c09579c2dcfa027282179cd2e466dc38f4
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-12-03 12:01:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,4 +26,13 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 }
 
-invokeAction(argv);
+const start = async (argv) => {
+    try {
+        await invokeAction(argv);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start(argv);
+
